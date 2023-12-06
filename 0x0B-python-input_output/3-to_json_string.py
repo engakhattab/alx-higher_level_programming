13d8b8ca7233650648cd196fed1ecf36508bb39f
engakhattab <engabdelrahmankhattab@gmail.com>
2023-12-06 21:26:54 +0000

    l

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-from json import dumps
+"""
+    3-to_json_string: to_json_string()
+"""
+
+
+import json
 
 
 def to_json_string(my_obj):
-    return dumps(my_obj)
+    """
+        returns the JSON representation of an object.
+    """
+    return (json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-from json import loads
+"""
+    6-load_from_json_file: load_from_json_file()
+"""
+
+
+import json
 
 
 def load_from_json_file(filename):
-    with open(filename, 'r') as f:
-        return loads(f.read())
+    """
+        loads an object to from JSON file.
+        Args:
+            filename (str): name of file.
+    """
+    with open(filename, "r") as j_file:
+        my_obj = json.load(j_file)
+        return my_obj
